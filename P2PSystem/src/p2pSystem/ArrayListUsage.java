edc3dbf62cf0317ee87e804202ad235553ee2e6e
SpecialCong <specialcong@gmail.com>
2015-02-26 19:30:07 -0800

    P2P Java

---
+++ P2PSystem/src/p2pSystem/ArrayListUsage.java
@@ -0,0 +1,15 @@
+package p2pSystem;
+import java.util.ArrayList;;
+public class ArrayListUsage {
+	public ArrayList<FileInfo> al ;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayListUsage a = new ArrayListUsage();
+		a.al = new ArrayList<FileInfo>();
+		
+	}
+
+}
